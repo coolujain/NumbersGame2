7ce04280b0e97c391acebe862b132e78e636d4f7
ftm-2005 <fyk@andrew.cmu.edu>
2025-06-15 17:50:26 +0300

    Update twoisprime.lean, shorter proof and different layout for statement

--- Game/Levels/Prime/twoisprime.lean
+++ Game/Levels/Prime/twoisprime.lean
@@ -19,26 +19,29 @@ Introduction
 "
   In this level, we will prove that 2 is prime.
 "
-/-- `two_is_prime` is a proof that 2 is prime .-/
-TheoremDoc MyNat.two_is_prime as "two_is_prime" in "∣"
 
 def prime (n :  ℕ) : Prop :=
   2 <= n ∧ ∀ m, m ∣ n → m = 1 ∨ m = n
 
       
-Statement two_is_prime
-   : prime 2 := by
-  unfold prime
-  constructor
-  apply le_refl
-  intro m hm 
-  have h2 : 2 ≠ 0 := succ_ne_zero 1
-  apply dvd_ls m 2 hm at h2 
-  apply le_two at h2
-  cases h2 with hzero hrest
-  rw [hzero] at hm
-  apply zero_dvd at hm
-  rw [two_eq_succ_one] at hm
-  apply succ_ne_zero at hm
-  tauto
-  exact hrest
+/-- `is_prime2 ` is a proof that `2 is a prime number`.-/
+TheoremDoc MyNat.is_prime2 as "is_prime2" in "∣"
+
+Statement is_prime2
+    (m : ℕ) : (2 ≤ 2) ∧ ( m ∣ 2 →  m = 1 ∨ m = 2) := by
+    constructor
+    decide
+    intro h2
+    have h4 :2 ≠ 0 := (succ_ne_zero 1) -- when we add the hint add it for this line and the line under by telling them to use dvd_le and use have
+    apply dvd_le m 2 h2 at h4
+    apply le_two at h4 -- hint..?
+    cases h4
+    rw[h] at h2
+    apply zero_dvd at h2
+    tauto
+    exact h
+
+Conclusion
+"
+  Congratulations! You have proven your first theorem about division.
+"
