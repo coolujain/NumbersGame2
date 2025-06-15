14547a69eecb00a5fa185ea5a02a9d4249b77da4
coolujain <143888770+coolujain@users.noreply.github.com>
2025-06-15 17:41:37 +0300

    Create Prime.lean

---
+++ Game/Levels/Prime.lean
@@ -0,0 +1,25 @@
+import Game.Levels.Division.L01one_dvd
+import Game.Levels.Division.L02dvd_refl
+import Game.Levels.Division.L03dvd_zero
+import Game.Levels.Division.L04dvd_one
+import Game.Levels.Division.L05dvd_trans
+import Game.Levels.Division.L06zero_dvd
+import Game.Levels.Division.L07dvd_le
+import Game.Levels.Division.L08dvd_antisymm
+import Game.Levels.Division.L09dvd_add_right
+import Game.Levels.Division.L10dvd_mul_right
+import Game.Levels.Division.L11dvd_mul_both
+import Game.Levels.Division.L12dvd_not_eq
+
+
+World "Prime"
+Title "Prime World"
+
+Introduction
+"
+  This is the world in which we define prime numbers. We will also prove
+  standard facts about prime numbers.
+
+  The definition of a prime number is that n is prime if \"2 <= n ∧ ∀ m, m ∣ n → m = 1 ∨ m = n\" .
+  Click on \"Start\" to proceed.
+"
