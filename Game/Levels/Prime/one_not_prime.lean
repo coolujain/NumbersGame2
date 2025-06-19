d3cea376f010ff53956c946b9961180a47cf0f3b
coolujain <143888770+coolujain@users.noreply.github.com>
2025-06-19 12:56:52 +0300

    Create one_not_prime.lean

---
+++ Game/Levels/Prime/one_not_prime.lean
@@ -0,0 +1,29 @@
+import Game.Levels.Multiplication
+import Game.Levels.AdvMultiplication
+import Game.MyNat.Division
+import Game.MyNat.PeanoAxioms
+import Game.Levels.LessOrEqual.L11le_two
+import Game.Levels.Division.L02dvd_refl
+import Game.Levels.Division.L06zero_dvd
+import Game.Levels.Division.L07dvd_ls
+
+World "Prime"
+Level 4
+Title "one_not_prime"
+
+TheoremTab "∣"
+
+namespace MyNat
+
+Introduction
+"
+  In this level, we will prove that one is not prime.
+"
+/-- `one_not_prime` is a proof that 1 is not prime .-/
+TheoremDoc MyNat.one_not_prime as "one_not_prime" in "∣"
+
+
+Statement one_not_prime
+  : (∃ y, y ∣ 1 ∧ y ≠ 1 ∧ y ≠ 1)  ∨ (1<=2) := by
+  right
+  decide
