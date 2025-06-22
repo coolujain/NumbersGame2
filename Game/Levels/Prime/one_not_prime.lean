cb20da8c5c4668776ca2a0cc14d3d1f54fc14745
coolujain <143888770+coolujain@users.noreply.github.com>
2025-06-22 13:58:18 +0300

    Update one_not_prime.lean

--- Game/Levels/Prime/one_not_prime.lean
+++ Game/Levels/Prime/one_not_prime.lean
@@ -12,7 +12,7 @@ World "Prime"
 Level 4
 Title "one_not_prime"
 
-TheoremTab "∣"
+TheoremTab "prime"
 
 namespace MyNat
 
@@ -21,7 +21,7 @@ Introduction
   In this level, we will prove that one is not prime.
 "
 /-- `one_not_prime` is a proof that 1 is not prime .-/
-TheoremDoc MyNat.one_not_prime as "one_not_prime" in "∣"
+TheoremDoc MyNat.one_not_prime as "one_not_prime" in "prime"
 
 
 Statement one_not_prime
